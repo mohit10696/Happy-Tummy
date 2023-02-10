abbcbcf4a17d3cbeb1383f9aae7c9107566bb48a
msojitra <mh546676@dal.ca>
2023-02-10 00:05:48 -0400

    Springboot project added

---
+++ happy-tummy-backend/src/main/java/com/happytummy/happytummybackend/HappyTummyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.happytummy.happytummybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTummyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyTummyBackendApplication.class, args);
+    }
+
+}
