0c33e48302e58ea8fb639c7c45fcc6f7c129f0b7
mohit sojitra <mh546676@dal.ca>
2023-02-21 03:04:35 -0400

    Dashboard API integrated

---
+++ HappyTummyFrontend/src/app/shared/constants/api.constant.ts
@@ -0,0 +1,4 @@
+export class APINAME {
+    // get Recipes
+    public static get GET_RECIPES(): string { return '/recipes' }
+}
---
+++ HappyTummyFrontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    API_URL: 'https://iondash.io/api/v1',
+  };
