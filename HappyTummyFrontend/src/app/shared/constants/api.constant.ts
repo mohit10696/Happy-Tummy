33000d87058034e962499c915aa7f7913ea0c2e2
Mohit Rajeshkumar Sojitra <mh546676@dal.ca>
2023-02-26 23:13:20 -0400

    Revert "recipe recommendation module frontend and backend"
    This reverts commit 9f78f2b24538ee413fe811c23b8d96aeec0f6695

--- HappyTummyFrontend/src/app/shared/constants/api.constant.ts
+++ HappyTummyFrontend/src/app/shared/constants/api.constant.ts
@@ -1,5 +1,4 @@
 export class APINAME {
     // get Recipes
     public static get GET_RECIPES(): string { return '/recipes' }
-    public static get GET_INGREDIENTS(): string { return '/ingredients' }
 }
