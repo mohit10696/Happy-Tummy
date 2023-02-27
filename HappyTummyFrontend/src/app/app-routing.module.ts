1731f1073d756242c2bb4d3cb6043cda772e8ee0
vikramv <vk485591@dal.ca>
2023-02-27 11:10:24 -0400

    Login and Sign-up page created - Routing from Dashboard working

--- HappyTummyFrontend/src/app/app-routing.module.ts
+++ HappyTummyFrontend/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     loadChildren: () => import('./pages/recipe-detail/recipe-detail.module').then(m => m.RecipeDetailModule)
   },
   {
+    path: 'authentication',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
